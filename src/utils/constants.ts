8caf9965bc856600cdf62ee499a6a88d02fa8ab5
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2024-08-15 19:53:57 +0400

    add file structure

---
+++ src/utils/constants.ts
@@ -0,0 +1,9 @@
+import ILink from "@/interfaces/_common/ILink"
+
+export const HEADER_HEIGHT = 80
+export const SIDEBAR_LINKS: ILink[] = [
+    {
+        href: '/',
+        title: "Home"
+    }
+]
